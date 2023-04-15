075a705d798aa3e252162f16e98f95f8a6fcf49e
Shrabon Ahamed <shrabonahamed106@gmail.com>
2023-04-16 02:34:08 +0600

    added navar and folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './Layout/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is App component</h1>
+      <Main></Main>
     </div>
   );
 }
